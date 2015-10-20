f61fdc3ff447f32aab713e99018c2c5e9ef241df
masterlan01 <masterlan@gmail.com>
2015-10-20 15:20:15 +0300

    change PostController add action "addPost"

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -5,7 +5,24 @@ class PostController {
     static scaffold = true
 
     def timeline = {
-        def user = User.findByUserId(params.id) // Отыскать пользователя по параметру id
+        def user = User.findById(params.id) // Отыскать пользователя по параметру id
         [ user : user ]                         // Передать пользователя в представление
+    }
+
+    def addPost = {
+        def user = User.findById(params.id) // Отыскивает пользователя по значению id
+        if (user) {
+            def post = new Post(params) // Запись данных в новый объект Post
+            user.addToPosts(post) // Связывает новое сообщение с пользователем
+            if (user.save()) { // save() вернет false, если проверка в объекте Post окончится неудачей
+                flash.message = "Successfully created Post"
+            } else {
+                user.discard()
+                flash.message = "Invalid or empty post"
+            }
+        } else {
+            flash.message = "Invalid User Id"
+        }
+        redirect(action: 'timeline', id: params.id) // Возвращает пользователяк списку сообщений
     }
 }

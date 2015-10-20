7ab430cd8191c494e505fe486215f89722db80a4
masterlan01 <masterlan@gmail.com>
2015-10-20 13:52:53 +0300

    commit PostController

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -1,5 +1,11 @@
 package com.grailsinaction
 
 class PostController {
+
     static scaffold = true
+
+    def timeline = {
+        def user = User.findByUserId(params.id) // Отыскать пользователя по параметру id
+        [ user : user ]                         // Передать пользователя в представление
+    }
 }
